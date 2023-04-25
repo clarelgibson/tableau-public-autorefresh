1ff5dec925a8c9aa3849408e4b1f8dca2017a01c
Clare Gibson <clarelgibson@gmail.com>
2023-04-25 12:02:25 +0100

    Initialise prepData script

---
+++ prepData.R
@@ -0,0 +1,13 @@
+# Title:        Prep Data
+# Project:      Tableau Public Auto-refresh
+# Author:       Clare Gibson
+# Date Created: 2023-04-25
+
+# SUMMARY ######################################################################
+# This script creates and exports data needed for the project.
+
+# SETUP ########################################################################
+# > Scripts ====================================================================
+source("utils.R")
+
+# GET DATA #####################################################################
