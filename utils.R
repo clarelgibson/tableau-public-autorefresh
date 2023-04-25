a6556dc6a1f74f05dc7d5865f7ed2dc739ea8e65
Clare Gibson <clarelgibson@gmail.com>
2023-04-25 12:36:44 +0100

    Adds mkdir function

--- utils.R
+++ utils.R
@@ -7,7 +7,7 @@
 # This script contains formulas needed to run the other scripts
 
 # MAKE TEST DATA ###############################################################
-# Function creates a df containing test data for the Tableau Public dashboard.
+# Function creates a df containing test data for the Tableau Public dashboard
 make_test_data <- function() {
   df <- data.frame(
     timestamp = Sys.time(),
@@ -19,7 +19,24 @@ make_test_data <- function() {
 }
 
 # GOOGLE AUTHENTICATION ########################################################
+# Function allows user to authenticate with Google Drive and Google Sheets
 google_auth <- function() {
   require(googledrive)
   require(googlesheets4)
+  
+  drive_auth()
+  gs4_auth(token = drive_token())
+}
+
+# CREATE DIRECTORY #############################################################
+# Function creates a new directory for this project in Google Drive. Checks
+# first for presence of directory with specified name. If exists, does not 
+# create a new directory.
+google_mkdir <- function(name) {
+  require(googledrive)
+  
+  drive_mkdir(
+    name = name,
+    overwrite = TRUE
+  )
 }
