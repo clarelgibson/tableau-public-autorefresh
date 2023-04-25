bc2778d9b290bc880d58d331bd62506199a672c8
Clare Gibson <clarelgibson@gmail.com>
2023-04-25 22:04:14 +0100

    Completes script to build custom functions

--- utils.R
+++ utils.R
@@ -32,11 +32,39 @@ google_auth <- function() {
 # Function creates a new directory for this project in Google Drive. Checks
 # first for presence of directory with specified name. If exists, does not 
 # create a new directory.
-google_mkdir <- function(name) {
+make_gdrive_folder <- function(name) {
   require(googledrive)
   
-  drive_mkdir(
-    name = name,
-    overwrite = TRUE
-  )
+  # Check if folder already exists (0 = FALSE, >0 = TRUE)
+  dir_flg <- nrow(drive_find(name))
+  
+  if (dir_flg == 0) {
+    drive_mkdir(name) %>% 
+      drive_share_anyone()
+    cat("Folder", name, "created successfully.")
+  } else {
+    cat("Folder", name, "already exists. No need to recreate.")
+  }
+}
+  
+# CREATE SHEET ###############################################################
+# Creates a new Google spreadsheet file for this project in Google Drive.
+# Checks first for presence of file with specified name. If exists, does not
+# create a new file.
+make_gdrive_sheet <- function(name, path) {
+  require(googledrive)
+    
+  # Check if file already exists (0 = FALSE, >0 = TRUE)
+  sheet_flg <- nrow(drive_find(name))
+  
+  if (sheet_flg == 0) {
+    drive_create(
+      name = name,
+      path = path,
+      type = "spreadsheet"
+    )
+    cat("Spreadsheet", name, "created successfully.")
+  } else {
+    cat("Spreadsheet", name, "already exists. No need to recreate.")
+  }
 }
