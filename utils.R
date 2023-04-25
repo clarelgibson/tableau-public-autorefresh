640354b92eab5e5ff782bd87bd051812514adc33
Clare Gibson <clarelgibson@gmail.com>
2023-04-25 12:01:53 +0100

    Initialise utils.R with make_test_data function

---
+++ utils.R
@@ -0,0 +1,19 @@
+# Title:        Utils
+# Project:      Tableau Public Auto-refresh
+# Author:       Clare Gibson
+# Date Created: 2023-04-25
+
+# SUMMARY ######################################################################
+# This script contains formulas needed to run the other scripts
+
+# MAKE TEST DATA ###############################################################
+# Function creates a df containing test data for the Tableau Public dashboard.
+make_test_data <- function() {
+  df <- data.frame(
+    timestamp = Sys.time(),
+    label = 1:10,
+    value = rnorm(10)
+  )
+  
+  return(df)
+}
